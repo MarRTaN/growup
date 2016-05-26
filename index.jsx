eb4d8bfda3fd30788d472fb47a1ee58ae482b898
selseo <silversoul.sevendeadlysins@gmail.com>
2016-05-26 09:27:32 +0000

    add user

--- index.jsx
+++ index.jsx
@@ -2,14 +2,16 @@ require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {HomeContainer} from './component/home'
+// import {HomeContainer} from './component/home'
+import {NewUserContainer} from './component/newUser'
+
 // const Test = ({data}) => (<h1 className="test"> test 2 ww  {data}</h1>)
 
 export class App extends React.Component {
 	render() {
 		return (
 
-			<HomeContainer />
+			<NewUserContainer />
 		);
 	}
 }
