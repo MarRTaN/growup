8493e5b3a39bf2e94f9807ea746a5ed8a8b2e036
selseo <silversoul.sevendeadlysins@gmail.com>
2016-05-26 08:10:22 +0000

    new port

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8081', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server',
+		'./index.jsx' // Your appʼs entry point
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	devServer: {
+		contentBase: "./public",
+			noInfo: true, //  --no-info option
+			hot: true,
+			inline: true,
+			port: process.env.PORT,
+			host: process.env.IP
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
+};
