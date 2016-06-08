b228e2995f6d0163b87826a80560b0b4cd7e718a
veha.suwat <veha.suwat@gmail.com>
2016-06-08 16:59:35 +0700

    mod route

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 
 import { CheckTreePage } from './component/checkTree'
 import { NewUserPage } from './component/newUser' //complete
-import { HomeContainer } from './component/home'
+import { HomePage } from './component/home'
 import { IntroContainer } from './component/intro'
 import { MarketContainer } from './component/market'
 import { ScanQRContainer } from './component/scanqr'
@@ -16,7 +16,7 @@ import { FriendContainer } from './component/friend'
 import { ProfileContainer } from './component/profile'
 import { TreeProfileContainer } from './component/treeprofile'
 import { NewDiaryAddedContainer } from './component/newdiaryadded'
-import { NewDiaryContainer } from './component/newdiary'
+import { NewDiaryContainer } from './component/newdiaryform'
 import { NewTreeAnimationContainer } from './component/newtreeanimation'
 import { ViewDiaryContainer } from './component/viewdiary'
 import { ViewDiaryListContainer } from './component/viewdiarylist'
@@ -41,6 +41,6 @@ ReactDOM.render((
     <Route path="/viewdiary" component={ViewDiaryContainer} />
     <Route path="/viewdiarylist" component={ViewDiaryListContainer} />
     <Route path="*" component={HomePage} />
-    
+
   </Router>
   ), document.querySelector('#myApp'))
